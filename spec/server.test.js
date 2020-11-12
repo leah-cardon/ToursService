cc1822acdf2a77e4c7b2516b521ab387a1614587
mikatpt <mikatpt@gmail.com>
2020-11-11 20:14:38 -0500

    Tweaked tests to work with mongo. Added coverage script

--- spec/server.test.js
+++ spec/server.test.js
@@ -4,7 +4,15 @@ const app = require('../server/app.js');
 
 const request = supertest(app);
 
-beforeAll((done) => done());
+beforeAll(async (done) => {
+  // disconnect from the real database first!
+  await mongoose.disconnect();
+
+  mongoose.connect('mongodb://localhost/tourDbTest', { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on('error', (err) => done.fail(err));
+  db.once('open', () => done());
+});
 
 describe('Test root path', () => {
   it('should respond to GET method', async (done) => request.get('/')
@@ -19,6 +27,6 @@ describe('Test root path', () => {
 });
 
 afterAll((done) => {
-  mongoose.connection.close();
+  mongoose.disconnect();
   done();
 });
