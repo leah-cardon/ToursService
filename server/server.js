c787c6cd3d78df6cdc8719e8993867e816b245d3
mikatpt <mikatpt@gmail.com>
2020-11-11 19:52:10 -0500

    Refactored server for testing purposes - moved app.listen

--- server/server.js
+++ server/server.js
@@ -1,24 +1,5 @@
-const express = require('express');
-
-const app = express();
-
-const path = require('path');
-
-const bodyParser = require('body-parser');
-
-const db = require('../database/models.js');
+const app = require('./app.js');
 
 const port = 3000;
-
-app.use(express.static(path.join(__dirname, '/../client/public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// req.body should include {name, number, email, type, date, time, call}
-// Inserts a user's tour request to the database
-app.post('/api/tours/users', (req, res) => db.insertUser(req.body, res));
-
-// Gets list of all agents and send to the client.
-app.get('/api/tours/agents', (req, res) => db.getAgents(res));
 
 app.listen(port, () => console.log(`Tours module is listening at http://localhost:${port}`));
