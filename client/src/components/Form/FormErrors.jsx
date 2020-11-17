09e9caa64a773e4ff045b85c857ae555a395c736
mikatpt <mikatpt@gmail.com>
2020-11-17 14:23:24 -0500

    Pushing this branch.

--- client/src/components/Form/FormErrors.jsx
+++ client/src/components/Form/FormErrors.jsx
@@ -4,36 +4,25 @@ import PropTypes from 'prop-types';
 
 import { errExcl } from '../../utils/SVG';
 
-/*
-TODO:
-Finish errors for email.
-
-*/
-
-const FormErrors = ({ name, number, email }) => {
-  const hi = 'hi';
-  return (
-    <div>
-
-      {name || number ? (
-        <div id="errNamePhone">
-          {name ? (
-            <div className="formErr">{errExcl}<p className="errText">Enter your name.</p></div>
-          ) : null }
-          {number ? (
-            <div className="formErr" id="errPhone">{errExcl}
-              <p className="errText">Enter a valid phone<br />number.</p>
-            </div>
-          ) : null }
-        </div>
-      ) : null}
-      {email ? (
-        <div className="formErr">{errExcl}<p className="errEmail">Enter a valid email.</p></div>
-      ) : null}
-    </div>
-
-  );
-};
+const FormErrors = ({ name, number, email }) => (
+  <div>
+    {name || number ? (
+      <div id="errNamePhone">
+        {name ? (
+          <div className="formErr">{errExcl}<p className="errText">Enter your name.</p></div>
+        ) : null }
+        {number ? (
+          <div className="formErr" id="errPhone">{errExcl}
+            <p className="errText">Enter a valid phone<br />number.</p>
+          </div>
+        ) : null }
+      </div>
+    ) : null}
+    {email ? (
+      <div className="formErr">{errExcl}<p className="errEmail">Enter a valid email.</p></div>
+    ) : null}
+  </div>
+);
 
 FormErrors.propTypes = {
   name: PropTypes.bool.isRequired,
