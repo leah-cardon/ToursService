fb10fe58e14d4113b008f518a0e8e6e1342aed60
mikatpt <mikatpt@gmail.com>
2020-11-18 17:58:15 -0500

    Removed bodyparser (redundant)

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const db = require('../database/models.js');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../client/public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
