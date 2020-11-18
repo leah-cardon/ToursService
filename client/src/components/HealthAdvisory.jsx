1ab343504b7431d2a3974916d9f5ce3b16f87dfe
mikatpt <mikatpt@gmail.com>
2020-11-17 19:41:37 -0500

    Added Health advisory

---
+++ client/src/components/HealthAdvisory.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+import ReactTooltip from 'react-tooltip';
+import { caution } from '../utils/SVG';
+
+const text = 'In-person tours may not currently allow for safe\nsocial distancing or comply with public health orders';
+
+const HealthAdvisory = () => (
+  <div>
+    <p id="healthWarning" data-tip data-for="healthToolTip">{caution} Public Health Advisory</p>
+
+    <ReactTooltip id="healthToolTip" className="opaque" place="top" effect="solid" type="light">{text}</ReactTooltip>
+  </div>
+);
+
+export default HealthAdvisory;
